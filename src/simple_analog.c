3af2b3455e00d72e31024a7446e6ce62275d4d32
aryaabraham <arya.register@gmail.com>
2015-06-04 23:38:54 -0400

    Added C code for weather

--- src/simple_analog.c
+++ src/simple_analog.c
@@ -3,24 +3,40 @@
 #include "pebble.h"
 
 static Window *window;
+
+// Text Time Data
 static Layer *s_simple_bg_layer, *s_time_layer; 
 static TextLayer *s_time_a_label, *s_time_b_label, *s_time_c_label;
-
 static const char *str_null = "";
 static const char *s_time_a_buffer = NULL, *s_time_b_buffer = NULL, *s_time_c_buffer = NULL;
-
-/* A function that prints given number in words */
-/* The first string is not used, it is to make array indexing simple */
 static const char *single_digits[] = { "o'clock", "one", "two", "three", "four",
                          "five", "six", "seven", "eight", "nine"};
-/* The first string is not used, it is to make array indexing simple */
 static const char *two_digits_a[] = {"ten", "eleven", "twelve", "thir", "four",
                       "fifteen", "sixteen", "seven", "eigh", "nine"};
 static const char *two_digits_b[] = {"", "", "", "teen", "teen",
                       "", "", "teen", "teen", "teen"};
-/* The first two string are not used, they are to make array indexing simple*/
 static const char *tens_multiple[] = {"", "", "twenty", "thirty", "forty", "fifty",
                          "sixty", "seventy", "eighty", "ninety"};
+
+// Weather Data
+static TextLayer *s_temperature_layer;
+static BitmapLayer *s_icon_layer;
+static GBitmap *s_icon_bitmap = NULL;
+static AppSync s_sync;
+static uint8_t s_sync_buffer[64];
+enum WeatherKey {
+  WEATHER_ICON_KEY = 0x0,         // TUPLE_INT
+  WEATHER_TEMPERATURE_KEY = 0x1,  // TUPLE_CSTRING
+  WEATHER_CITY_KEY = 0x2,         // TUPLE_CSTRING
+};
+static const uint32_t WEATHER_ICONS[] = {
+  RESOURCE_ID_IMAGE_SUN, //0
+  RESOURCE_ID_IMAGE_CLOUD, //1
+  RESOURCE_ID_IMAGE_RAIN, //2
+  RESOURCE_ID_IMAGE_SNOW //3
+};
+
+// Text Time Code
 static void convert_to_words(const char **str_top, const char **str_bot, int num)
 {
     int tens = num/10;
@@ -49,12 +65,6 @@ static void convert_to_words(const char **str_top, const char **str_bot, int num
     }
 }
 
-static void bg_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
-  graphics_context_set_fill_color(ctx, GColorWhite);
-}
-
 static void time_update_proc(Layer *layer, GContext *ctx) {
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
@@ -67,6 +77,59 @@ static void time_update_proc(Layer *layer, GContext *ctx) {
   text_layer_set_text(s_time_b_label, s_time_b_buffer);
   text_layer_set_text(s_time_c_label, s_time_c_buffer);
   //APP_LOG(APP_LOG_LEVEL_INFO, "%s %s", s_time_b_buffer, s_time_c_buffer);
+}
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+  switch (key) {
+    case WEATHER_ICON_KEY:
+      if (s_icon_bitmap) {
+        gbitmap_destroy(s_icon_bitmap);
+      }
+
+      s_icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[new_tuple->value->uint8]);
+
+#ifdef PBL_SDK_3
+      bitmap_layer_set_compositing_mode(s_icon_layer, GCompOpSet);
+#endif
+      
+      bitmap_layer_set_bitmap(s_icon_layer, s_icon_bitmap);
+      break;
+
+    case WEATHER_TEMPERATURE_KEY:
+      // App Sync keeps new_tuple in s_sync_buffer, so we may use it directly
+      text_layer_set_text(s_temperature_layer, new_tuple->value->cstring);
+      break;
+
+    case WEATHER_CITY_KEY:
+      //text_layer_set_text(s_city_layer, new_tuple->value->cstring);
+      break;
+  }
+}
+
+static void request_weather(void) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+
+  if (!iter) {
+    // Error creating outbound message
+    return;
+  }
+
+  int value = 1;
+  dict_write_int(iter, 1, &value, sizeof(int), true);
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+}
+
+static void bg_update_proc(Layer *layer, GContext *ctx) {
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
+  graphics_context_set_fill_color(ctx, GColorWhite);
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
@@ -82,6 +145,7 @@ static void window_load(Window *window) {
   layer_set_update_proc(s_simple_bg_layer, bg_update_proc);
   layer_add_child(window_layer, s_simple_bg_layer);
 
+  // Text Time
   s_time_layer = layer_create(bounds);
   layer_set_update_proc(s_time_layer, time_update_proc);
   layer_add_child(window_layer, s_time_layer);
@@ -110,6 +174,31 @@ static void window_load(Window *window) {
   text_layer_set_text_color(s_time_c_label, GColorWhite);
   text_layer_set_font(s_time_c_label, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT)); 
   layer_add_child(s_time_layer, text_layer_get_layer(s_time_c_label));
+  
+  // Weather
+  s_icon_layer = bitmap_layer_create(GRect(124, 144, 20, 20));
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_icon_layer));
+
+  s_temperature_layer = text_layer_create(GRect(80, 142, 44, 20));
+  text_layer_set_text_color(s_temperature_layer, COLOR_FALLBACK(GColorBlack, GColorWhite));
+  text_layer_set_background_color(s_temperature_layer, GColorClear);
+  text_layer_set_font(s_temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text_alignment(s_temperature_layer, GTextAlignmentRight);
+  layer_add_child(window_layer, text_layer_get_layer(s_temperature_layer));
+  
+  // Weather
+  Tuplet initial_values[] = {
+    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
+    TupletCString(WEATHER_TEMPERATURE_KEY, "12\u00B0F"),
+    TupletCString(WEATHER_CITY_KEY, "St Pebblesburg"),
+  };
+
+  app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer), 
+      initial_values, ARRAY_LENGTH(initial_values),
+      sync_tuple_changed_callback, sync_error_callback, NULL
+  );
+
+  request_weather();
 }
 
 static void window_unload(Window *window) {
@@ -119,6 +208,12 @@ static void window_unload(Window *window) {
   text_layer_destroy(s_time_a_label);
   text_layer_destroy(s_time_b_label);
   text_layer_destroy(s_time_c_label);
+  
+  if (s_icon_bitmap) {
+    gbitmap_destroy(s_icon_bitmap);
+  }
+  bitmap_layer_destroy(s_icon_layer);
+  text_layer_destroy(s_temperature_layer);
 }
 
 static void init() {
@@ -131,11 +226,17 @@ static void init() {
   window_stack_push(window, true);
 
   tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  
+  // Weather
+  app_message_open(64, 64);
 }
 
 static void deinit() {
   tick_timer_service_unsubscribe();
   window_destroy(window);
+  
+  // Weather
+  app_sync_deinit(&s_sync);
 }
 
 int main() {
